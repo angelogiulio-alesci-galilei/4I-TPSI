e834866e6fed776c9dce5cda79ed9e8625dd0096
angelogiulio-alesci-galilei <angelogiulio.alesci@galileiostiglia.edu.it>
2025-03-18 18:19:38 +0000

    18-03-25

--- 4 Parallelismo Soluzioni/5 - 9 /5 esecuzione non deterministica/2_sol.c
+++ 4 Parallelismo Soluzioni/5 - 9 /5 esecuzione non deterministica/2_sol.c
@@ -6,36 +6,27 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-//	Le parti commentate non si possono fare perche' i case dello switch devono essere costanti, non possono essere variabili
-
-int main () {
+int main() {
+	printf("1 Sono il padre  .. \n");
+  
 	int pid;
-	//int pidMain, pid1, pid2;
 	pid = fork();
-	if (pid==0)
-		pid = 1;
-		//pid = pid1 = getpid();
-	else {
-		pid = fork();
-		if (pid==0)
-			pid = 2;
-			//pid = pid2 = getpid();
-		else
-			pid = 0;
-			//pid = pidMain = getpid();
-	}
-
-	switch (pid) {
-		case 0:		//case pidMain:
-			printf("Processo padre con PID: %d\n",pid);
-			break;
-		case 1:		//case pid1:
-			printf("Processo 1o con PID: %d\n",pid);
-			exit(1);
-			break;
-		case 2:		//case pid2:
-			printf("Processo 2o con PID: %d\n",pid);
-			exit(2);
-			break;
+  
+	switch(pid){
+	  	case -1:
+			printf("Errore in creazione figlio\n");
+	      	return(-1);
+	    case  0:					// figlio 
+	    	printf("2 Figlio si riposa  .. \n");
+	    	sleep(2);
+	    	printf("3 Figlio termina\n");
+	    	exit(0);				// termina il processo figlio                      
+	    default:
+			printf("4 Padre aspetta che termini il figlio...\n");
+			sleep(3);
+			printf("5 Padre termina \n");
+			exit(1);				// termina il processo padre  
 	}
 }
+
+
